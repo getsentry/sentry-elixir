2e3c9178f04437614174c42924477de3bd72b7a0
Mitchell Henke <mitchh23@gmail.com>
2016-08-04 16:02:29 -0500

    move auth test

---
+++ test/client_test.exs
@@ -0,0 +1,9 @@
+defmodule Sentry.ClientTest do
+  use ExUnit.Case
+  alias Sentry.Client
+
+  test "authorization" do
+    {_endpoint, public_key, private_key} = Sentry.parse_dsn!("https://public:secret@app.getsentry.com/1")
+    assert Client.authorization_header(public_key, private_key) =~ ~r/Sentry sentry_version=5, sentry_client=sentry-elixir\/0.0.5, sentry_timestamp=\d+, sentry_key=public, sentry_secret=secret/
+  end
+end
