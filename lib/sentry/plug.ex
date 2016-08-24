9bbaf3d6f056ad84ca9ad3113233f8bbc43ec4cf
Jason Stiebs <jason@rokkincat.com>
2016-08-24 16:20:29 -0500

    Allow scrubbing of params and provide some default scrubbing for headers

--- lib/sentry/plug.ex
+++ lib/sentry/plug.ex
@@ -1,18 +1,59 @@
 defmodule Sentry.Plug do
   @moduledoc """
-  TODO: module doc
+  Provides basic funcitonality to handle Plug.ErrorHandler
+
+  #### Usage
+
+  Add the following to your router.ex:
+      
+      use Plug.ErrorLogger
+      use Sentry.Plug
+
+  ### Sending Post Body Params
+
+  In order to send post body parameters you need to first scrub them of sensitive information. To
+  do so we ask you to pass a `scrubber` key which accepts a `Plug.Conn` and returns a map with keys 
+  to send. 
+
+      def scrub_params(conn) do
+        conn.params # Make sure the params have been fetched.
+        |> Map.to_list
+        |> Enum.filter(fn ({key, val} -> 
+          key in ~w(password passwd secret credit_card) ||
+          Regex.match?(~r(^(?:\d[ -]*?){13,16}$, val) # Matches Credit Cards
+        end)
+        |> Enum.into(%{})
+      end
+
+  Then pass it into Sentry.Plug
+
+      use Sentry.Plug, scrubber: scrub_params\1
+
+  You can also pass it in as a `{module, fun}` like so
+
+      use Sentry.Plug, scrubber: {MyModule, :scrub_params}
+
+  ### Headers Scrubber
+
+  By default we will scrub Authorization and Authentication headers from all requests before sending them. 
+
   """
-  defmacro __using__(_env) do
+
+
+
+  defmacro __using__(env) do
+    scrubber = Keyword.get(env, :scrubber, nil)
+
     quote do
       defp handle_errors(conn, %{kind: kind, reason: reason, stack: stack}) do
-        request = Sentry.Plug.build_request_interface_data(conn)
+        request = Sentry.Plug.build_request_interface_data(conn, unquote(scrubber))
         exception = Exception.normalize(kind, reason, stack)
         Sentry.capture_exception(exception, [stacktrace: stack, request: request])
       end
     end
   end
 
-  def build_request_interface_data(%Plug.Conn{} = conn) do
+  def build_request_interface_data(%Plug.Conn{} = conn, scrubber) do
     conn = conn
             |> Plug.Conn.fetch_cookies
             |> Plug.Conn.fetch_query_params
@@ -20,11 +61,10 @@ defmodule Sentry.Plug do
     %{
       url: "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}",
       method: conn.method,
-      data: %{
-      },
+      data: handle_request_data(conn, scrubber),
       query_string: conn.query_string,
       cookies: conn.req_cookies,
-      headers: Enum.into(conn.req_headers, %{}),
+      headers: Enum.into(conn.req_headers, %{}) |> scrub_headers(),
       env: %{
         "REMOTE_ADDR" => remote_address(conn.remote_ip),
         "REMOTE_PORT" => remote_port(conn.peer),
@@ -41,4 +81,16 @@ defmodule Sentry.Plug do
   end
 
   def remote_port({_, port}), do: port
+
+  defp handle_request_data(_conn, nil), do: %{}
+  defp handle_request_data(conn, {module, fun}) do
+    apply(module, fun, [conn])
+  end
+  defp handle_request_data(conn, fun) when is_function(fun) do
+    fun.(conn)
+  end
+
+  defp scrub_headers(data) do
+    Map.drop(data, ~w(authorization authentication)a)
+  end
 end
