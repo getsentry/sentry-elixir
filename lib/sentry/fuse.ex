2a66d70699961ef3b49ee7434f067d51faedeb3f
Mitchell Henke <mitchh23@gmail.com>
2016-08-25 15:20:43 -0500

    add fuse moduledoc

--- lib/sentry/fuse.ex
+++ lib/sentry/fuse.ex
@@ -4,6 +4,14 @@ defmodule Sentry.Fuse do
   @failure_period 5_000
   @reset_period 30_000
 
+  @moduledoc """
+  To avoid sending requests to the Sentry API during periods of failure, we use
+  a circuit breaker to update and check the health of the API. Currently, the
+  fuse is configured to melt if the API returns unsuccessful responses 5 times
+  within 5 seconds.  Following a melt, the fuse will be reset and API attempts
+  will be allowed again after a 30 second reset period.
+  """
+
   def install_fuse do
     :fuse.install(:sentry_api, {{:standard, @max_failures, @failure_period}, {:reset, @reset_period}})
   end
