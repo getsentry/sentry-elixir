0929ddbaf8741893fea144669544d30cc661671f
Mitchell Henke <mitchh23@gmail.com>
2016-08-26 15:59:50 -0500

    add test client

---
+++ test/support/test_client.ex
@@ -0,0 +1,7 @@
+defmodule Sentry.TestClient do
+  def send_event(%Sentry.Event{} = event) do
+    {endpoint, _public_key, _secret_key} = Sentry.Client.parse_dsn!(Application.fetch_env!(:sentry_elixir, :dsn))
+    body = Poison.encode!(event)
+    :hackney.request(:post, endpoint, [], body, [])
+  end
+end
