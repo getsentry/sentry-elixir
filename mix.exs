9faea6d8dd984f8ccedd92cf3e803cbbf827a758
Jason Stiebs <jason@rokkincat.com>
2016-09-08 10:24:56 -0500

    Small updates to the docs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Sentry.Mixfile do
       app: :sentry,
       version: "1.0.0",
       elixir: "~> 1.3",
-      description: "An Elixir client for Sentry",
+      description: "The Official Elixir client for Sentry",
       package: package(),
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env),
