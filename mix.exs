5cdb12f313090cc5608280efcc28bf745d7d9fc2
Mitchell Henke <mitchh23@gmail.com>
2016-11-15 17:53:01 -0600

    release 1.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sentry.Mixfile do
   def project do
     [
       app: :sentry,
-      version: "1.1.1",
+      version: "1.1.2",
       elixir: "~> 1.3",
       description: "The Official Elixir client for Sentry",
       package: package(),
