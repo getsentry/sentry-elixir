74887b6ac8d7ad9d6c15f81bbe19aafd46c0b80b
Mitchell Henke <mitchh23@gmail.com>
2017-03-01 11:23:04 -0600

    avoid poison raising exception

--- test/support/test_client.ex
+++ test/support/test_client.ex
@@ -1,7 +1,11 @@
 defmodule Sentry.TestClient do
   def send_event(%Sentry.Event{} = event) do
     {endpoint, _public_key, _secret_key} = Sentry.Client.get_dsn!
-    body = Poison.encode!(event)
-    Sentry.Client.request(:post, endpoint, [], body)
+    case Poison.encode(event) do
+      {:ok, body} ->
+        Sentry.Client.request(:post, endpoint, [], body)
+      {:error, _error} ->
+        :error
+    end
   end
 end
