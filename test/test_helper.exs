8303e06719b2873a62e449df0ea73e1626422df9
Mitchell Henke <mitchh23@gmail.com>
2017-08-03 12:18:41 -0500

    redo test support files

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,9 @@
+Code.load_file("test/support/test_plug.ex")
+Code.require_file("test/support/test_environment_helper.exs")
+Code.require_file("test/support/test_before_send_event.exs")
+Code.require_file("test/support/test_filter.exs")
+Code.require_file("test/support/test_gen_server.exs")
+Code.require_file("test/support/test_client.exs")
+
 ExUnit.start()
 Application.ensure_all_started(:bypass)
