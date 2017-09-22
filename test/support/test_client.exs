d69fc02585a41698c2e6a1b7a3805f1615dac3af
Mitchell Henke <mitchh23@gmail.com>
2017-09-22 10:18:39 -0500

    log errors in testclient

--- test/support/test_client.exs
+++ test/support/test_client.exs
@@ -1,5 +1,6 @@
 defmodule Sentry.TestClient do
   @behaviour Sentry.HTTPClient
+  require Logger
 
   def send_event(%Sentry.Event{} = event, _opts \\ []) do
     {endpoint, _public_key, _secret_key} = Sentry.Client.get_dsn!
@@ -7,7 +8,8 @@ defmodule Sentry.TestClient do
     case Poison.encode(event) do
       {:ok, body} ->
         Sentry.Client.request(:post, endpoint, [], body)
-      {:error, _error} ->
+      {:error, error} ->
+        Logger.error("Error sending in Sentry.TestClient: #{inspect(error)}")
         :error
     end
   end
