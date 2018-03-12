45b1d7f236af54c495682972db8d61d1d11d2010
Mitchell Henke <mitchh23@gmail.com>
2018-03-12 12:54:18 -0500

    placeholder

--- lib/sentry/phoenix_endpoint.ex
+++ lib/sentry/phoenix_endpoint.ex
@@ -1,4 +1,17 @@
 defmodule Sentry.Phoenix.Endpoint do
+  @moduledoc """
+  Provides basic functionality to handle errors in a Phoenix Endpoint. Errors occurring within a Phoenix request before it reaches the Router will not be captured by `Sentry.Plug` due to the internal workings of Phoenix.
+
+  For more information, see https://github.com/getsentry/sentry-elixir/issues/229.
+
+
+  #### Usage
+
+  Add the following to your endpoint.ex, below `use Phoenix.Endpoint, otp_app: :my_app`
+
+        use Sentry.Phoenix.Endpoint
+
+  """
   defmacro __using__(_opts) do
     quote do
       @before_compile Sentry.Phoenix.Endpoint
