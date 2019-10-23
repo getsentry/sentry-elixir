e7c0d1afb3be33e8c278b45b8bd1251fef6e6145
Mitchell Henke <mitchell@mitchellhenke.com>
2019-10-23 10:50:37 -0500

    remove credo

--- mix.exs
+++ mix.exs
@@ -31,7 +31,6 @@ defmodule Sentry.Mixfile do
       {:phoenix, "~> 1.3", optional: true},
       {:dialyxir, "> 0.0.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.20.0", only: :dev},
-      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:bypass, "~> 1.0", only: [:test]}
     ]
   end
