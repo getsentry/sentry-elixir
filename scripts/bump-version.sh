d7247c88d57139dc95ca60fb3cd0a54e3ec1a020
Neel Shah <neelshah.sa@gmail.com>
2023-01-27 18:26:59 +0100

    Add craft with target hex

---
+++ scripts/bump-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux
+
+SCRIPT_DIR="$( dirname "$0" )"
+cd $SCRIPT_DIR/..
+
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
+
+echo "Current version: $OLD_VERSION"
+echo "Bumping version: $NEW_VERSION"
+
+function replace() {
+    ! grep "$2" $3
+    perl -i -pe "s/$1/$2/g" $3
+    grep "$2" $3  # verify that replacement was successful
+}
+
+replace "\@version \"[0-9.]+\"" "\@version \"$NEW_VERSION\"" ./mix.exs
