082b887bbc8e4ac878c32ad0734e8de0b2b462c3
Andrea Leopardi <an.leopardi@gmail.com>
2023-07-29 11:50:55 +0200

    Polish docs for Sentry.EventFilter

--- lib/sentry/default_event_filter.ex
+++ lib/sentry/default_event_filter.ex
@@ -1,8 +1,4 @@
 defmodule Sentry.DefaultEventFilter do
-  @behaviour Sentry.EventFilter
-
-  @moduledoc false
-
   @ignored_exceptions [
     Phoenix.NotAcceptableError,
     Phoenix.Router.NoRouteError,
@@ -14,11 +10,20 @@ defmodule Sentry.DefaultEventFilter do
     Plug.Static.InvalidPathError
   ]
 
-  def exclude_exception?(%x{}, :plug) when x in @ignored_exceptions do
-    true
-  end
+  @moduledoc """
+  The default implementation of the `Sentry.EventFilter` behaviour.
 
+  This filter excludes the following exceptions:
+
+  #{Enum.map_join(@ignored_exceptions, "\n", &"  * `#{inspect(&1)}`")}
+
+  In addition, it excludes routes that do not match in plug routers.
+  """
+
+  @behaviour Sentry.EventFilter
+
+  @impl true
+  def exclude_exception?(%x{}, :plug) when x in @ignored_exceptions, do: true
   def exclude_exception?(%FunctionClauseError{function: :do_match, arity: 4}, :plug), do: true
-
   def exclude_exception?(_, _), do: false
 end
