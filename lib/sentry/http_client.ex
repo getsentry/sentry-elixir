9cbde0efe9f29174b1babf6094a6359b6fb1650d
Andrea Leopardi <an.leopardi@gmail.com>
2023-08-01 17:29:59 +0200

    Polish docs for HTTP client (#576)

--- lib/sentry/http_client.ex
+++ lib/sentry/http_client.ex
@@ -1,21 +1,45 @@
 defmodule Sentry.HTTPClient do
   @moduledoc """
-  Specifies the API for using a custom HTTP Client.
+  A behaviour for HTTP clients that Sentry can use.
 
-  The default HTTP client is `Sentry.HackneyClient`
+  The default HTTP client is `Sentry.HackneyClient`.
 
   To configure a different HTTP client, implement the `Sentry.HTTPClient` behaviour and
   change the `:client` configuration:
 
       config :sentry,
         client: MyHTTPClient
+
   """
 
+  @typedoc """
+  The response status for an HTTP request.
+  """
+  @typedoc since: "9.0.0"
+  @type status :: 100..599
+
+  @typedoc """
+  HTTP request or response headers.
+  """
   @type headers :: [{String.t(), String.t()}]
 
+  @typedoc """
+  HTTP request or response body.
+  """
+  @typedoc since: "9.0.0"
+  @type body :: binary()
+
+  @doc """
+  Should return a **child specification** to start the HTTP client.
+
+  For example, this can start a pool of HTTP connections dedicated to Sentry.
+  """
   @callback child_spec() :: :supervisor.child_spec()
 
-  @callback post(url :: String.t(), headers, body :: String.t()) ::
-              {:ok, status :: pos_integer, headers, body :: String.t()}
-              | {:error, term}
+  @doc """
+  Should make an HTTP `POST` request to `url` with the given `headers` and `body`.
+  """
+  @callback post(url :: String.t(), request_headers :: headers(), request_body :: body()) ::
+              {:ok, status(), response_headers :: headers(), response_body :: body()}
+              | {:error, term()}
 end
