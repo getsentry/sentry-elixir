6f8f61045f6ee609bd027ce96627b81f3c3ca360
Andrea Leopardi <an.leopardi@gmail.com>
2023-08-01 19:09:51 +0200

    Add compile-time configuration (#579)

--- test/config_test.exs
+++ test/config_test.exs
@@ -25,19 +25,8 @@ defmodule Sentry.ConfigTest do
   end
 
   describe "source_code_path_pattern" do
-    test "retrieves from environment" do
-      modify_env(:sentry, source_code_path_pattern: "**/*test.ex")
-      assert "**/*test.ex" == Config.source_code_path_pattern()
-    end
-
     test "returns default when not configured" do
       assert "**/*.ex" == Config.source_code_path_pattern()
-    end
-
-    test "does not retrieve from DSN" do
-      dsn = "https://public:super_secret@app.getsentry.com/2?source_code_path_pattern=test"
-      modify_env(:sentry, dsn: dsn)
-      refute "test" == Config.source_code_path_pattern()
     end
   end
 
@@ -45,31 +34,6 @@ defmodule Sentry.ConfigTest do
     test "retrieves from app env" do
       modify_env(:sentry, included_environments: [:test, :dev])
       assert [:test, :dev] == Config.included_environments()
-    end
-  end
-
-  describe "root_source_code_paths" do
-    test "raises error if :root_source_code_paths is not set" do
-      delete_env(:sentry, [:root_source_code_paths])
-
-      expected_error_message = ":root_source_code_paths must be configured"
-
-      assert_raise ArgumentError, expected_error_message, fn ->
-        Config.root_source_code_paths()
-      end
-    end
-
-    test "returns :root_source_code_paths if it's set" do
-      modify_env(:sentry, root_source_code_path: nil)
-      modify_env(:sentry, root_source_code_paths: ["/test"])
-
-      assert Config.root_source_code_paths() == ["/test"]
-    end
-
-    test "call to :root_source_code_paths does not read dsn env" do
-      modify_env(:sentry, dsn: {:system, "DSN", required: true})
-
-      Config.root_source_code_paths()
     end
   end
 end
