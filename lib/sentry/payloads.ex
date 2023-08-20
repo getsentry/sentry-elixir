3e0e56803fa6518aa30743a20489adcd1ad3d29e
Andrea Leopardi <an.leopardi@gmail.com>
2023-08-20 07:58:56 +0200

    WIP

--- lib/sentry/payloads.ex
+++ lib/sentry/payloads.ex
@@ -2,9 +2,15 @@ defmodule Sentry.Payloads do
   @moduledoc false
 
   defmodule Event do
-    @moduledoc false
-    # https://develop.sentry.dev/sdk/event-payloads/
+    @moduledoc """
+    The struct for the top-level Sentry event payload.
 
+    See <https://develop.sentry.dev/sdk/event-payloads>.
+    """
+
+    @moduledoc since: "9.0.0"
+
+    @typedoc since: "9.0.0"
     @type t() :: %__MODULE__{
             # Required
             event_id: <<_::128>>,
@@ -19,10 +25,11 @@ defmodule Sentry.Payloads do
             release: String.t() | nil,
             dist: String.t() | nil,
             tags: %{optional(String.t()) => String.t()},
-            environment: String.t(),
+            environment: String.t() | nil,
             modules: %{optional(String.t()) => String.t()},
             extra: map(),
             fingerprint: [String.t()],
+            sdk: Sentry.Payloads.SDK.t() | nil,
 
             # Possible payloads.
             exception: [Sentry.Payloads.Exception.t(), ...] | nil
@@ -47,6 +54,7 @@ defmodule Sentry.Payloads do
       :release,
       :dist,
       :exception,
+      :sdk,
 
       # Required. Has to be "elixir".
       platform: :elixir,
@@ -60,10 +68,37 @@ defmodule Sentry.Payloads do
     ]
   end
 
-  defmodule Exception do
-    @moduledoc false
-    # https://develop.sentry.dev/sdk/event-payloads/exception/
+  defmodule SDK do
+    @moduledoc """
+    The struct for the **SDK** interface.
 
+    This is usually filled in by the SDK itself.
+
+    See <https://develop.sentry.dev/sdk/event-payloads/sdk>.
+    """
+
+    @moduledoc since: "9.0.0"
+
+    @typedoc since: "9.0.0"
+    @type t() :: %__MODULE__{
+            name: String.t(),
+            version: String.t()
+          }
+
+    @enforce_keys [:name, :version]
+    defstruct [:name, :version]
+  end
+
+  defmodule Exception do
+    @moduledoc """
+    The struct for the **exception** interface.
+
+    See <https://develop.sentry.dev/sdk/event-payloads/exception>.
+    """
+
+    @moduledoc since: "9.0.0"
+
+    @typedoc since: "9.0.0"
     @type t() :: %__MODULE__{
             type: String.t(),
             value: String.t(),
@@ -76,9 +111,15 @@ defmodule Sentry.Payloads do
   end
 
   defmodule Stacktrace.Frame do
-    @moduledoc false
-    # https://develop.sentry.dev/sdk/event-payloads/stacktrace/
+    @moduledoc """
+    The struct for the **stacktrace frame** to be used within exceptions.
 
+    See `Sentry.Payloads.Stacktrace`.
+    """
+
+    @moduledoc since: "9.0.0"
+
+    @typedoc since: "9.0.0"
     @type t() :: %__MODULE__{
             filename: Path.t(),
             function: String.t(),
@@ -97,9 +138,15 @@ defmodule Sentry.Payloads do
   end
 
   defmodule Stacktrace do
-    @moduledoc false
-    # https://develop.sentry.dev/sdk/event-payloads/stacktrace/
+    @moduledoc """
+    The struct for the **stacktrace** interface.
 
+    See <https://develop.sentry.dev/sdk/event-payloads/stacktrace>.
+    """
+
+    @moduledoc since: "9.0.0"
+
+    @typedoc since: "9.0.0"
     @type t() :: %__MODULE__{
             frames: [Sentry.Payloads.Stacktrace.Frame.t()]
           }
