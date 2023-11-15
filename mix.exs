97302e855b80ba687ecc7ef6f8985216a8d52017
Andrea Leopardi <an.leopardi@gmail.com>
2023-11-15 14:20:28 +0100

    Add some flags to Dialyzer

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Sentry.Mixfile do
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
+        flags: [:unmatched_returns, :error_handling, :extra_return],
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
         plt_core_path: "priv/plts",
         plt_add_deps: :app_tree,
