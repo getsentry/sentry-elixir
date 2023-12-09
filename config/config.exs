23e388fdf57993a881566ca82512d3eca60222fc
Andrea Leopardi <an.leopardi@gmail.com>
2023-12-09 16:39:27 +0100

    FIXUP

--- config/config.exs
+++ config/config.exs
@@ -16,14 +16,3 @@ if config_env() == :test do
 end
 
 config :phoenix, :json_library, Jason
-
-config :sentry,
-  dsn:
-    "https://b3af41aceb36bd71d100d67b3b0af63b@o4506347701469184.ingest.sentry.io/4506347702779904",
-  environment_name: :prod,
-  enable_source_code_context: true,
-  root_source_code_paths: [File.cwd!()],
-  tags: %{
-    env: "production"
-  },
-  included_environments: [:prod]
