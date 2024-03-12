42cc5d6f99a11e8129d30c7183511952586fd77f
Andrea Leopardi <an.leopardi@gmail.com>
2024-03-12 13:54:50 +0100

    Support RCs in bump-version.sh script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-SCRIPT_DIR="$( dirname "$0" )"
+SCRIPT_DIR="$(dirname "$0")"
 cd $SCRIPT_DIR/..
 
 OLD_VERSION="${1}"
@@ -11,9 +11,20 @@ echo "Current version: $OLD_VERSION"
 echo "Bumping version: $NEW_VERSION"
 
 function replace() {
-    ! grep "$2" $3
-    perl -i -pe "s/$1/$2/g" $3
-    grep "$2" $3  # verify that replacement was successful
+    local _path="$3"
+
+    if grep "$2" "$_path" >/dev/null; then
+        echo "Version already bumped to $NEW_VERSION"
+        exit 1
+    fi
+
+    perl -i -pe "s/$1/$2/g" "$3"
+
+    # Verify that replacement was successful
+    if ! grep "$2" "$3"; then
+        echo "Failed to bump version"
+        exit 1
+    fi
 }
 
-replace "\@version \"[0-9.]+\"" "\@version \"$NEW_VERSION\"" ./mix.exs
+replace "\@version \"[0-9.rc\-]+\"" "\@version \"$NEW_VERSION\"" ./mix.exs
