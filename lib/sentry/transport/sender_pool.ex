77debb7a2af806222e961ad21cde3cdc782d01bb
Andrea Leopardi <an.leopardi@gmail.com>
2024-04-14 17:31:39 +0200

    Fix race condition with :sender_pool_size
    Closes #709.

--- lib/sentry/transport/sender_pool.ex
+++ lib/sentry/transport/sender_pool.ex
@@ -10,17 +10,24 @@ defmodule Sentry.Transport.SenderPool do
 
   @impl true
   def init([]) do
-    pool_size = max(System.schedulers_online(), 8)
-
-    Application.put_env(:sentry, :sender_pool_size, pool_size)
-
     children =
-      for index <- 1..pool_size do
+      for index <- 1..pool_size() do
         Supervisor.child_spec({Sentry.Transport.Sender, index: index},
           id: {Sentry.Transport.Sender, index}
         )
       end
 
     Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  @spec pool_size() :: pos_integer()
+  def pool_size do
+    if pool_size = :persistent_term.get({:sentry, :sender_pool_size}, nil) do
+      pool_size
+    else
+      value = max(System.schedulers_online(), 8)
+      :persistent_term.put({:sentry, :sender_pool_size}, value)
+      value
+    end
   end
 end
