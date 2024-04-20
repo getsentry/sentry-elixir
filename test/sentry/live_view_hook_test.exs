ddc8ccb01a0ff86660a5a45d89e76ce30d2a6f14
Andrea Leopardi <an.leopardi@gmail.com>
2024-04-20 10:10:26 +0200

    FIXUP

--- test/sentry/live_view_hook_test.exs
+++ test/sentry/live_view_hook_test.exs
@@ -1,133 +1,136 @@
-defmodule SentryTest.ErrorView do
-  def render(template, assigns) do
-    dbg()
-    "OK"
-  end
-end
-
-defmodule SentryTest.Live do
-  use Phoenix.LiveView
-
-  on_mount Sentry.LiveViewHook
-
-  def render(assigns) do
-    ~H"""
-    <h1>Testing Sentry hooks</h1>
-    """
+# TODO: Oban requires Elixir 1.13+, remove this once we depend on that too.
+if Version.match?(System.version(), "~> 1.13") do
+  defmodule SentryTest.ErrorView do
+    def render(template, assigns) do
+      dbg()
+      "OK"
+    end
   end
 
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  defmodule SentryTest.Live do
+    use Phoenix.LiveView
+
+    on_mount Sentry.LiveViewHook
+
+    def render(assigns) do
+      ~H"""
+      <h1>Testing Sentry hooks</h1>
+      """
+    end
+
+    def mount(_params, _session, socket) do
+      {:ok, socket}
+    end
+
+    def handle_event("refresh", _params, socket) do
+      {:noreply, socket}
+    end
+
+    def handle_info(:test_message, socket) do
+      {:noreply, socket}
+    end
   end
 
-  def handle_event("refresh", _params, socket) do
-    {:noreply, socket}
+  defmodule SentryTest.Router do
+    use Phoenix.Router
+    import Phoenix.LiveView.Router
+
+    scope "/" do
+      live "/hook_test", SentryTest.Live
+    end
   end
 
-  def handle_info(:test_message, socket) do
-    {:noreply, socket}
-  end
-end
+  defmodule SentryTest.Endpoint do
+    use Phoenix.Endpoint, otp_app: :sentry
 
-defmodule SentryTest.Router do
-  use Phoenix.Router
-  import Phoenix.LiveView.Router
+    socket "/live", Phoenix.LiveView.Socket,
+      websocket: [connect_info: [:peer_data, :uri, :user_agent]]
 
-  scope "/" do
-    live "/hook_test", SentryTest.Live
-  end
-end
-
-defmodule SentryTest.Endpoint do
-  use Phoenix.Endpoint, otp_app: :sentry
-
-  socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [:peer_data, :uri, :user_agent]]
-
-  plug SentryTest.Router
-end
-
-defmodule Sentry.LiveViewHookTest do
-  use Sentry.Case, async: true
-
-  import Phoenix.ConnTest
-  import Phoenix.LiveViewTest
-
-  @endpoint SentryTest.Endpoint
-
-  setup_all do
-    Application.put_env(:sentry, SentryTest.Endpoint,
-      secret_key_base: "TMnue44VMTf1VmyD6SYKR30cqKpHluHOFZGXcVkC33hKVVKTVZ1HBQLLLLLLLLLL",
-      live_view: [signing_salt: "F8ftIAbYdeTzhwgl"]
-    )
-
-    pid = start_supervised!(SentryTest.Endpoint)
-    Process.link(pid)
-    :ok
+    plug SentryTest.Router
   end
 
-  setup do
-    %{conn: build_conn()}
-  end
+  defmodule Sentry.LiveViewHookTest do
+    use Sentry.Case, async: true
 
-  test "attaches the right context", %{conn: conn} do
-    conn =
-      conn
-      |> Plug.Conn.put_req_header("user-agent", "sentry-testing 1.0")
+    import Phoenix.ConnTest
+    import Phoenix.LiveViewTest
 
-    {:ok, view, html} = live(conn, "/hook_test")
-    assert html =~ "<h1>Testing Sentry hooks</h1>"
+    @endpoint SentryTest.Endpoint
 
-    context1 = get_sentry_context(view)
+    setup_all do
+      Application.put_env(:sentry, SentryTest.Endpoint,
+        secret_key_base: "TMnue44VMTf1VmyD6SYKR30cqKpHluHOFZGXcVkC33hKVVKTVZ1HBQLLLLLLLLLL",
+        live_view: [signing_salt: "F8ftIAbYdeTzhwgl"]
+      )
 
-    assert "phx-" <> _ = context1.extra.socket_id
-    assert context1.request.url == "http://www.example.com/hook_test"
-    assert context1.request.user_agent == "sentry-testing 1.0"
+      pid = start_supervised!(SentryTest.Endpoint)
+      Process.link(pid)
+      :ok
+    end
 
-    assert [
-             %{category: "web.live_view.params"} = params_breadcrumb,
-             %{category: "web.live_view.mount"} = mount_breadcrumb
-           ] = context1.breadcrumbs
+    setup do
+      %{conn: build_conn()}
+    end
 
-    assert mount_breadcrumb.message == "Mounted live view"
-    assert mount_breadcrumb.data == %{}
+    test "attaches the right context", %{conn: conn} do
+      conn =
+        conn
+        |> Plug.Conn.put_req_header("user-agent", "sentry-testing 1.0")
 
-    assert params_breadcrumb.message == "http://www.example.com/hook_test"
-    assert params_breadcrumb.data == %{params: %{}, uri: "http://www.example.com/hook_test"}
+      {:ok, view, html} = live(conn, "/hook_test")
+      assert html =~ "<h1>Testing Sentry hooks</h1>"
 
-    # Send an event and test the new breadcrumb.
+      context1 = get_sentry_context(view)
 
-    assert render_hook(view, :refresh, %{force: true}) =~ "Testing Sentry hooks"
+      assert "phx-" <> _ = context1.extra.socket_id
+      assert context1.request.url == "http://www.example.com/hook_test"
+      assert context1.request.user_agent == "sentry-testing 1.0"
 
-    context2 = get_sentry_context(view)
-    assert Map.take(context1, [:extra, :request]) == Map.take(context2, [:extra, :request])
-    assert [event_breadcrumb, ^params_breadcrumb, ^mount_breadcrumb] = context2.breadcrumbs
-    assert event_breadcrumb.category == "web.live_view.event"
-    assert event_breadcrumb.message == ~s("refresh")
-    assert event_breadcrumb.data == %{params: %{"force" => true}, event: "refresh"}
+      assert [
+               %{category: "web.live_view.params"} = params_breadcrumb,
+               %{category: "web.live_view.mount"} = mount_breadcrumb
+             ] = context1.breadcrumbs
 
-    # Send a message and test the new breadcrumb.
-    send(view.pid, :test_message)
-    assert render(view) =~ "Testing Sentry hooks"
+      assert mount_breadcrumb.message == "Mounted live view"
+      assert mount_breadcrumb.data == %{}
 
-    context3 = get_sentry_context(view)
-    assert Map.take(context1, [:extra, :request]) == Map.take(context3, [:extra, :request])
+      assert params_breadcrumb.message == "http://www.example.com/hook_test"
+      assert params_breadcrumb.data == %{params: %{}, uri: "http://www.example.com/hook_test"}
 
-    assert [info_breadcrumb, ^event_breadcrumb, ^params_breadcrumb, ^mount_breadcrumb] =
-             context3.breadcrumbs
+      # Send an event and test the new breadcrumb.
 
-    assert info_breadcrumb.category == "web.live_view.info"
-    assert info_breadcrumb.message == ~s(:test_message)
-  end
+      assert render_hook(view, :refresh, %{force: true}) =~ "Testing Sentry hooks"
 
-  defp get_sentry_context(view) do
-    {:dictionary, pdict} = Process.info(view.pid, :dictionary)
+      context2 = get_sentry_context(view)
+      assert Map.take(context1, [:extra, :request]) == Map.take(context2, [:extra, :request])
+      assert [event_breadcrumb, ^params_breadcrumb, ^mount_breadcrumb] = context2.breadcrumbs
+      assert event_breadcrumb.category == "web.live_view.event"
+      assert event_breadcrumb.message == ~s("refresh")
+      assert event_breadcrumb.data == %{params: %{"force" => true}, event: "refresh"}
 
-    assert {:ok, sentry_context} =
-             pdict
-             |> Keyword.fetch!(:"$logger_metadata$")
-             |> Map.fetch(Sentry.Context.__logger_metadata_key__())
+      # Send a message and test the new breadcrumb.
+      send(view.pid, :test_message)
+      assert render(view) =~ "Testing Sentry hooks"
 
-    sentry_context
+      context3 = get_sentry_context(view)
+      assert Map.take(context1, [:extra, :request]) == Map.take(context3, [:extra, :request])
+
+      assert [info_breadcrumb, ^event_breadcrumb, ^params_breadcrumb, ^mount_breadcrumb] =
+               context3.breadcrumbs
+
+      assert info_breadcrumb.category == "web.live_view.info"
+      assert info_breadcrumb.message == ~s(:test_message)
+    end
+
+    defp get_sentry_context(view) do
+      {:dictionary, pdict} = Process.info(view.pid, :dictionary)
+
+      assert {:ok, sentry_context} =
+               pdict
+               |> Keyword.fetch!(:"$logger_metadata$")
+               |> Map.fetch(Sentry.Context.__logger_metadata_key__())
+
+      sentry_context
+    end
   end
 end
