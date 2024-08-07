1bb5224053ebbb7dd193b3b6260fbb6473bc72d9
Savannah Manning <sm05908@gmail.com>
2024-08-07 17:54:42 +0200

    make genserver configurable for testing

--- test/sentry/check_in_id_mappings_test.exs
+++ test/sentry/check_in_id_mappings_test.exs
@@ -3,19 +3,28 @@ defmodule Sentry.CheckInIDMappingsTest do
   use Sentry.Case, async: false
 
   alias Sentry.Integrations.CheckInIDMappings
+  @table :sentry_cron_mappings
 
   describe "lookup_or_insert_new/1" do
     test "works correctly" do
       cron_key = "quantum_123"
 
+      child_spec = %{
+        id: TestMappings,
+        start:
+          {CheckInIDMappings, :start_link, [[max_expected_check_in_time: 0, name: TestMappings]]}
+      }
+
+      pid = start_supervised!(child_spec)
+
       CheckInIDMappings.lookup_or_insert_new(cron_key)
-      assert :ets.lookup(:sentry_cron_mappings, cron_key) != []
+      assert :ets.lookup(@table, cron_key) != []
 
       Process.sleep(5)
-      send(CheckInIDMappings, {:sweep, 0})
-      _ = :sys.get_state(CheckInIDMappings)
+      send(pid, :sweep)
+      _ = :sys.get_state(pid)
 
-      assert :ets.lookup(:sentry_cron_mappings, cron_key) == []
+      assert :ets.lookup(@table, cron_key) == []
     end
   end
 end
