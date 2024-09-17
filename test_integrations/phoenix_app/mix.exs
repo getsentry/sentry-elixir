c3d78a24ddf8a599ecc2569defbf8605d14ef1e2
Peter Solnica <peter@solnica.online>
2024-09-17 07:33:22 +0000

    Add full e2e phoenix app test

---
+++ test_integrations/phoenix_app/mix.exs
@@ -0,0 +1,80 @@
+defmodule PhoenixApp.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :phoenix_app,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {PhoenixApp.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:nimble_options, "~> 1.0"},
+      {:nimble_ownership, "~> 0.3.0 or ~> 1.0"},
+
+      {:phoenix, "~> 1.7.14"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 4.1"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
+      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:swoosh, "~> 1.5"},
+      {:finch, "~> 0.13"},
+      {:telemetry_metrics, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.20"},
+      {:jason, "~> 1.2"},
+      {:dns_cluster, "~> 0.1.1"},
+      {:bandit, "~> 1.5"},
+      {:bypass, "~> 2.1", only: :test},
+      {:hackney, "~> 1.18", only: :test},
+
+      {:sentry, path: "../.."}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind phoenix_app", "esbuild phoenix_app"],
+      "assets.deploy": [
+        "tailwind phoenix_app --minify",
+        "esbuild phoenix_app --minify",
+        "phx.digest"
+      ]
+    ]
+  end
+end
