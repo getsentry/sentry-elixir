4e8e7372dd8ebd900253270f96ca1e45d3d75b65
Peter Solnica <peter@solnica.online>
2024-12-09 12:36:56 +0000

    Add sweeping of expired spans

--- test/sentry/opentelemetry/span_storage_test.exs
+++ test/sentry/opentelemetry/span_storage_test.exs
@@ -95,14 +95,11 @@ defmodule Sentry.OpenTelemetry.SpanStorageTest do
       SpanStorage.store_span(child_span1)
       SpanStorage.store_span(child_span2)
 
-      # Verify initial state
       assert root_span == SpanStorage.get_root_span("root123")
       assert length(SpanStorage.get_child_spans("root123")) == 2
 
-      # Remove root span should remove everything
       SpanStorage.remove_span("root123")
 
-      # Verify everything is removed
       assert nil == SpanStorage.get_root_span("root123")
       assert [] == SpanStorage.get_child_spans("root123")
     end
@@ -217,5 +214,148 @@ defmodule Sentry.OpenTelemetry.SpanStorageTest do
 
     assert nil == SpanStorage.get_root_span("root123")
     assert [] == SpanStorage.get_child_spans("root123")
+  end
+
+  describe "stale span cleanup" do
+    test "cleans up stale spans" do
+      start_supervised!({SpanStorage, cleanup_interval: 100, name: :cleanup_test})
+
+      root_span = %SpanRecord{
+        span_id: "stale_root",
+        parent_span_id: nil,
+        trace_id: "trace123",
+        name: "stale_root_span"
+      }
+
+      child_span = %SpanRecord{
+        span_id: "stale_child",
+        parent_span_id: "stale_root",
+        trace_id: "trace123",
+        name: "stale_child_span"
+      }
+
+      old_time = System.system_time(:second) - :timer.minutes(31)
+      :ets.insert(:span_storage, {{:root_span, "stale_root"}, {root_span, old_time}})
+      :ets.insert(:span_storage, {"stale_root", {child_span, old_time}})
+
+      fresh_root_span = %SpanRecord{
+        span_id: "fresh_root",
+        parent_span_id: nil,
+        trace_id: "trace123",
+        name: "fresh_root_span"
+      }
+
+      SpanStorage.store_span(fresh_root_span)
+
+      Process.sleep(200)
+
+      assert nil == SpanStorage.get_root_span("stale_root")
+      assert [] == SpanStorage.get_child_spans("stale_root")
+
+      assert SpanStorage.get_root_span("fresh_root")
+    end
+
+    test "cleans up orphaned child spans" do
+      start_supervised!({SpanStorage, cleanup_interval: 100, name: :cleanup_test})
+
+      child_span = %SpanRecord{
+        span_id: "stale_child",
+        parent_span_id: "non_existent_parent",
+        trace_id: "trace123",
+        name: "stale_child_span"
+      }
+
+      old_time = System.system_time(:second) - :timer.minutes(31)
+      :ets.insert(:span_storage, {"non_existent_parent", {child_span, old_time}})
+
+      Process.sleep(200)
+
+      assert [] == SpanStorage.get_child_spans("non_existent_parent")
+    end
+
+    test "cleans up expired root spans with all their children regardless of child timestamps" do
+      start_supervised!({SpanStorage, cleanup_interval: 100, name: :cleanup_test})
+
+      root_span = %SpanRecord{
+        span_id: "root123",
+        parent_span_id: nil,
+        trace_id: "trace123",
+        name: "root_span"
+      }
+
+      old_child = %SpanRecord{
+        span_id: "old_child",
+        parent_span_id: "root123",
+        trace_id: "trace123",
+        name: "old_child_span"
+      }
+
+      fresh_child = %SpanRecord{
+        span_id: "fresh_child",
+        parent_span_id: "root123",
+        trace_id: "trace123",
+        name: "fresh_child_span"
+      }
+
+      old_time = System.system_time(:second) - :timer.minutes(31)
+      :ets.insert(:span_storage, {{:root_span, "root123"}, {root_span, old_time}})
+
+      :ets.insert(:span_storage, {"root123", {old_child, old_time}})
+      SpanStorage.store_span(fresh_child)
+
+      Process.sleep(200)
+
+      assert nil == SpanStorage.get_root_span("root123")
+      assert [] == SpanStorage.get_child_spans("root123")
+    end
+
+    test "handles mixed expiration times in child spans" do
+      start_supervised!({SpanStorage, cleanup_interval: 100, name: :cleanup_test})
+
+      root_span = %SpanRecord{
+        span_id: "root123",
+        parent_span_id: nil,
+        trace_id: "trace123",
+        name: "root_span"
+      }
+
+      old_child1 = %SpanRecord{
+        span_id: "old_child1",
+        parent_span_id: "root123",
+        trace_id: "trace123",
+        name: "old_child_span_1"
+      }
+
+      old_child2 = %SpanRecord{
+        span_id: "old_child2",
+        parent_span_id: "root123",
+        trace_id: "trace123",
+        name: "old_child_span_2"
+      }
+
+      fresh_child = %SpanRecord{
+        span_id: "fresh_child",
+        parent_span_id: "root123",
+        trace_id: "trace123",
+        name: "fresh_child_span"
+      }
+
+      SpanStorage.store_span(root_span)
+
+      old_time = System.system_time(:second) - :timer.minutes(31)
+      :ets.insert(:span_storage, {"root123", {old_child1, old_time}})
+      :ets.insert(:span_storage, {"root123", {old_child2, old_time}})
+
+      SpanStorage.store_span(fresh_child)
+
+      Process.sleep(200)
+
+      assert root_span == SpanStorage.get_root_span("root123")
+      children = SpanStorage.get_child_spans("root123")
+      assert length(children) == 1
+      assert fresh_child in children
+      refute old_child1 in children
+      refute old_child2 in children
+    end
   end
 end
