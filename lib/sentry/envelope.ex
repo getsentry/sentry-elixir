83b016263e288e24db5175bc9d698299464f6563
Peter Solnica <peter@solnica.online>
2024-12-13 16:53:18 +0000

    Support Transaction in client reports

--- lib/sentry/envelope.ex
+++ lib/sentry/envelope.ex
@@ -63,12 +63,20 @@ defmodule Sentry.Envelope do
   Returns the "data category" of the envelope's contents (to be used in client reports and more).
   """
   @doc since: "10.8.0"
-  @spec get_data_category(Attachment.t() | CheckIn.t() | ClientReport.t() | Event.t()) ::
+  @spec get_data_category(
+          Attachment.t()
+          | CheckIn.t()
+          | ClientReport.t()
+          | Event.t()
+          | Transaction.t()
+        ) ::
           String.t()
   def get_data_category(%Attachment{}), do: "attachment"
   def get_data_category(%CheckIn{}), do: "monitor"
   def get_data_category(%ClientReport{}), do: "internal"
   def get_data_category(%Event{}), do: "error"
+  # TODO: is this correct?
+  def get_data_category(%Transaction{}), do: "error"
 
   @doc """
   Encodes the envelope into its binary representation.
