901d184cbeb8cdd72723cec70a02186deccf09f7
Peter Solnica <peter@solnica.online>
2025-01-29 13:56:28 +0000

    Auto-discover mod names for in_app_module_allow_list

---
+++ test_integrations/umbrella/apps/public/test/public_test.exs
@@ -0,0 +1,8 @@
+defmodule Public.Sentry.ConfigTest do
+  use ExUnit.Case
+
+  test "loads in_app_module_allow_list" do
+    assert Sentry.Config.in_app_module_allow_list() |> Enum.sort() ==
+             [Admin, Admin.Settings, Public, Public.User]
+  end
+end
