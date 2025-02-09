8b979364e7f1c5cbf42c098b349c035c7be3f43c
Savannah Manning <sm05908@gmail.com>
2025-02-09 10:11:56 -0700

    Support per-module custom options for check ins (#833)
    Co-authored-by: Savannah Manning <smanning@Savannahs-MacBook-Pro.local>
    Co-authored-by: Andrea Leopardi <an.leopardi@gmail.com>

--- lib/sentry/integrations/oban/cron.ex
+++ lib/sentry/integrations/oban/cron.ex
@@ -1,6 +1,23 @@
 defmodule Sentry.Integrations.Oban.Cron do
-  @moduledoc false
+  @moduledoc """
+  This module provides built-in integration for cron jobs managed by Oban.
+  """
+
+  @moduledoc since: "10.9.0"
+
   alias Sentry.Integrations.CheckInIDMappings
+
+  @doc """
+  The Oban integration calls this callback (if present) to customize
+  the configuration options for the check-in.
+
+  This function must return options compatible with the ones passed to `Sentry.CheckIn.new/1`.
+
+  Options returned by this function overwrite any option inferred by the specific
+  integration for the check in. We perform *deep merging* of nested keyword options.
+  """
+  @doc since: "10.9.0"
+  @callback sentry_check_in_configuration(oban_job :: struct()) :: options_to_merge :: keyword()
 
   @events [
     [:oban, :job, :start],
@@ -8,12 +25,14 @@ defmodule Sentry.Integrations.Oban.Cron do
     [:oban, :job, :exception]
   ]
 
+  @doc false
   @spec attach_telemetry_handler(keyword()) :: :ok
   def attach_telemetry_handler(config) when is_list(config) do
     _ = :telemetry.attach_many(__MODULE__, @events, &__MODULE__.handle_event/4, config)
     :ok
   end
 
+  @doc false
   @spec handle_event([atom()], term(), term(), keyword()) :: :ok
   def handle_event(event, measurements, metadata, config)
 
@@ -87,13 +106,54 @@ defmodule Sentry.Integrations.Oban.Cron do
       monitor_config_opts ->
         id = CheckInIDMappings.lookup_or_insert_new(job.id)
 
-        [
+        opts = [
           check_in_id: id,
           # This is already a binary.
           monitor_slug: monitor_slug,
           monitor_config: monitor_config_opts
         ]
+
+        resolve_custom_opts(opts, job)
     end
+  end
+
+  defp resolve_custom_opts(opts, %{worker: worker} = job)
+       when is_struct(job, Oban.Job) and is_binary(worker) do
+    job.worker |> String.split(".") |> Module.safe_concat()
+  rescue
+    ArgumentError -> opts
+  else
+    worker ->
+      if Code.ensure_loaded?(worker) do
+        resolve_custom_opts(opts, worker, job)
+      else
+        opts
+      end
+  end
+
+  defp resolve_custom_opts(opts, _job) do
+    opts
+  end
+
+  defp resolve_custom_opts(options, mod, per_integration_term) do
+    custom_opts =
+      if function_exported?(mod, :sentry_check_in_configuration, 1) do
+        mod.sentry_check_in_configuration(per_integration_term)
+      else
+        []
+      end
+
+    deep_merge_keyword(options, custom_opts)
+  end
+
+  defp deep_merge_keyword(left, right) do
+    Keyword.merge(left, right, fn _key, left_val, right_val ->
+      if Keyword.keyword?(left_val) and Keyword.keyword?(right_val) do
+        deep_merge_keyword(left_val, right_val)
+      else
+        right_val
+      end
+    end)
   end
 
   defp schedule_opts(%{meta: meta} = job) when is_struct(job, Oban.Job) do
