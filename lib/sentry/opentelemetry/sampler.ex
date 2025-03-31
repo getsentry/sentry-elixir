33332bc4fd9a43d0abbb04542524c4bf423565cf
Peter Solnica <peter@solnica.online>
2025-03-31 10:05:01 +0000

    Skip loading Sampler if otel_sampler is not loaded

--- lib/sentry/opentelemetry/sampler.ex
+++ lib/sentry/opentelemetry/sampler.ex
@@ -1,27 +1,29 @@
-defmodule Sentry.OpenTelemetry.Sampler do
-  @moduledoc false
+if Code.ensure_loaded?(:otel_sampler) do
+  defmodule Sentry.OpenTelemetry.Sampler do
+    @moduledoc false
 
-  def setup(config) do
-    config
-  end
+    def setup(config) do
+      config
+    end
 
-  def description(_) do
-    "SentrySampler"
-  end
+    def description(_) do
+      "SentrySampler"
+    end
 
-  def should_sample(
-        _ctx,
-        _trace_id,
-        _links,
-        span_name,
-        _span_kind,
-        _attributes,
-        config
-      ) do
-    if span_name in config[:drop] do
-      {:drop, [], []}
-    else
-      {:record_and_sample, [], []}
+    def should_sample(
+          _ctx,
+          _trace_id,
+          _links,
+          span_name,
+          _span_kind,
+          _attributes,
+          config
+        ) do
+      if span_name in config[:drop] do
+        {:drop, [], []}
+      else
+        {:record_and_sample, [], []}
+      end
     end
   end
 end
