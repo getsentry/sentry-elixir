edd724c233c3f52b612ffac32e3f0792397e6d86
Peter Solnica <peter@solnica.online>
2025-04-03 10:40:43 +0000

    Make SpanStorage more robust

--- test/sentry/opentelemetry/span_storage_test.exs
+++ test/sentry/opentelemetry/span_storage_test.exs
@@ -228,7 +228,7 @@ defmodule Sentry.OpenTelemetry.SpanStorageTest do
       old_time = System.system_time(:second) - :timer.minutes(31)
 
       :ets.insert(table_name, {{:root_span, "stale_root"}, root_span, old_time})
-      :ets.insert(table_name, {"stale_root", {child_span, old_time}})
+      :ets.insert(table_name, {{:child_span, "stale_root", "stale_child"}, child_span, old_time})
 
       fresh_root_span = %SpanRecord{
         span_id: "fresh_root",
@@ -347,6 +347,142 @@ defmodule Sentry.OpenTelemetry.SpanStorageTest do
       assert fresh_child in children
       refute old_child1 in children
       refute old_child2 in children
+    end
+  end
+
+  describe "concurrent operations" do
+    @tag span_storage: true
+    test "handles concurrent span updates safely", %{table_name: table_name} do
+      root_span = %SpanRecord{
+        span_id: "root123",
+        parent_span_id: nil,
+        trace_id: "trace123",
+        name: "root_span"
+      }
+
+      SpanStorage.store_span(root_span, table_name: table_name)
+
+      tasks =
+        for i <- 1..10000 do
+          Task.async(fn ->
+            updated_span = %{root_span | name: "updated_name_#{i}"}
+            SpanStorage.update_span(updated_span, table_name: table_name)
+          end)
+        end
+
+      Task.await_many(tasks)
+
+      result = SpanStorage.get_root_span("root123", table_name: table_name)
+      assert result.span_id == "root123"
+      assert result.name =~ ~r/^updated_name_\d+$/
+    end
+
+    @tag span_storage: true
+    test "handles concurrent child span operations", %{table_name: table_name} do
+      parent_id = "parent123"
+
+      tasks =
+        for i <- 1..10000 do
+          Task.async(fn ->
+            child_span = %SpanRecord{
+              span_id: "child_#{i}",
+              parent_span_id: parent_id,
+              trace_id: "trace123",
+              name: "child_span_#{i}"
+            }
+
+            SpanStorage.store_span(child_span, table_name: table_name)
+          end)
+        end
+
+      Task.await_many(tasks)
+
+      children = SpanStorage.get_child_spans(parent_id, table_name: table_name)
+      assert length(children) == 10000
+      assert Enum.all?(children, &(&1.parent_span_id == parent_id))
+    end
+  end
+
+  describe "span timestamps" do
+    @tag span_storage: true
+    test "maintains correct timestamp ordering", %{table_name: table_name} do
+      now = System.system_time(:second)
+
+      spans =
+        for i <- 1..5 do
+          %SpanRecord{
+            span_id: "span_#{i}",
+            parent_span_id: "parent123",
+            trace_id: "trace123",
+            name: "span_#{i}",
+            start_time: now + i,
+            end_time: now + i + 10
+          }
+        end
+
+      Enum.reverse(spans)
+      |> Enum.each(&SpanStorage.store_span(&1, table_name: table_name))
+
+      retrieved_spans = SpanStorage.get_child_spans("parent123", table_name: table_name)
+      assert length(retrieved_spans) == 5
+
+      assert retrieved_spans
+             |> Enum.map(& &1.start_time)
+             |> Enum.chunk_every(2, 1, :discard)
+             |> Enum.all?(fn [a, b] -> a <= b end)
+    end
+  end
+
+  describe "cleanup" do
+    @tag span_storage: [cleanup_interval: 100]
+    test "cleanup respects span TTL precisely", %{table_name: table_name} do
+      now = System.system_time(:second)
+      ttl = :timer.minutes(30)
+
+      spans = [
+        {now - ttl - 1, "too_old"},
+        {now - ttl + 1, "just_fresh"},
+        {now - div(ttl, 2), "middle_aged"},
+        {now, "fresh"}
+      ]
+
+      Enum.each(spans, fn {timestamp, name} ->
+        span = %SpanRecord{
+          span_id: name,
+          parent_span_id: nil,
+          trace_id: "trace123",
+          name: name
+        }
+
+        :ets.insert(table_name, {{:root_span, name}, span, timestamp})
+      end)
+
+      Process.sleep(200)
+
+      assert nil == SpanStorage.get_root_span("too_old", table_name: table_name)
+      assert not is_nil(SpanStorage.get_root_span("just_fresh", table_name: table_name))
+      assert not is_nil(SpanStorage.get_root_span("middle_aged", table_name: table_name))
+      assert not is_nil(SpanStorage.get_root_span("fresh", table_name: table_name))
+    end
+
+    @tag span_storage: [cleanup_interval: 100]
+    test "cleanup handles large number of expired spans efficiently", %{table_name: table_name} do
+      old_time = System.system_time(:second) - :timer.minutes(31)
+
+      for i <- 1..10000 do
+        root_span = %SpanRecord{
+          span_id: "span_#{i}",
+          parent_span_id: nil,
+          trace_id: "trace123",
+          name: "span_#{i}"
+        }
+
+        :ets.insert(table_name, {{:root_span, "span_#{i}"}, root_span, old_time})
+      end
+
+      Process.sleep(200)
+
+      assert :ets.info(table_name, :size) == 0
     end
   end
 end
