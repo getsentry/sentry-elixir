e8f580ad83e5a3d9451689629092ff0f18029912
Andrea Leopardi <an.leopardi@gmail.com>
2025-05-15 18:32:19 +0200

    Add Telemetry integration

---
+++ lib/sentry/integrations/telemetry.ex
@@ -0,0 +1,63 @@
+defmodule Sentry.Integrations.Telemetry do
+  @moduledoc """
+  Sentry integration for Telemetry.
+
+  *Available since v10.10.0*.
+  """
+
+  @moduledoc since: "10.10.0"
+
+  @failure_event [:telemetry, :handler, :failure]
+
+  @doc false
+  @spec attach() :: :ok
+  def attach do
+    :telemetry.attach(
+      "#{inspect(__MODULE__)}-telemetry-failures",
+      @failure_event,
+      &handle_event/4,
+      :no_config
+    )
+
+    :ok
+  end
+
+  @doc false
+  @spec handle_event(
+          :telemetry.event_name(),
+          :telemetry.event_measurements(),
+          :telemetry.event_metadata(),
+          :telemetry.handler_config()
+        ) :: :ok
+  def handle_event(@failure_event, _measurements, %{} = metadata, :no_config) do
+    stacktrace = metadata[:stacktrace] || []
+
+    options = [
+      stacktrace: stacktrace,
+      tags: %{
+        telemetry_handler_id: stringified_handler_id(metadata[:handler_id]),
+        event_name: inspect(metadata[:event_name])
+      }
+    ]
+
+    case {metadata[:kind], metadata[:reason]} do
+      {:error, reason} ->
+        exception = Exception.normalize(:error, reason, stacktrace)
+        Sentry.capture_exception(exception, options)
+
+      {kind, reason} ->
+        options =
+          Keyword.merge(options,
+            extra: %{kind: inspect(kind), reason: inspect(reason)},
+            interpolation_parameters: [inspect(metadata[:handler_id])]
+          )
+
+        Sentry.capture_message("Telemetry handler %s failed", options)
+    end
+
+    :ok
+  end
+
+  defp stringified_handler_id(handler_id) when is_binary(handler_id), do: handler_id
+  defp stringified_handler_id(handler_id), do: inspect(handler_id)
+end
