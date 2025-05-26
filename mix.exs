9294eaf3aa82f69e68f652bf490eb384dfb2de30
Peter Solnica <peter@solnica.online>
2025-05-26 11:46:11 +0000

    Revert "Revert "Add SpanProcessor for OpenTelemetry (#875)""
    This reverts commit 2ced90ef993ac2aae3ffd49eaa27c485827fce51.

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule Sentry.Mixfile do
   def application do
     [
       mod: {Sentry.Application, []},
-      extra_applications: [:logger],
+      extra_applications: extra_applications(Mix.env()),
       registered: [
         Sentry.Dedupe,
         Sentry.Transport.SenderRegistry,
@@ -83,6 +83,9 @@ defmodule Sentry.Mixfile do
       ]
     ]
   end
+
+  defp extra_applications(:test), do: [:logger, :opentelemetry]
+  defp extra_applications(_other), do: [:logger]
 
   defp elixirc_paths(:test), do: ["test/support"] ++ elixirc_paths(:dev)
   defp elixirc_paths(_other), do: ["lib"]
@@ -113,7 +116,11 @@ defmodule Sentry.Mixfile do
       # Required by Phoenix.LiveView's testing
       {:floki, ">= 0.30.0", only: :test},
       {:oban, "~> 2.17 and >= 2.17.6", only: [:test]},
-      {:quantum, "~> 3.0", only: [:test]}
+      {:quantum, "~> 3.0", only: [:test]},
+      {:opentelemetry, "~> 1.5", optional: true},
+      {:opentelemetry_api, "~> 1.4", optional: true},
+      {:opentelemetry_exporter, "~> 1.0", optional: true},
+      {:opentelemetry_semantic_conventions, "~> 1.27", optional: true}
     ]
   end
 
