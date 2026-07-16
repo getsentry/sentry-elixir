12c25886441214619c63f58873cdebd6b641e57f
Peter Solnica <peter@solnica.online>
2026-07-16 12:33:09 +0200

    chore(deps): bump lockfiles + add new dedicated ones (#1120)
    We need them for integration tests too now, because version
    bumps diverged.

--- test_integrations/prod_mode/mix.exs
+++ test_integrations/prod_mode/mix.exs
@@ -6,6 +6,7 @@ defmodule ProdMode.MixProject do
       app: :prod_mode,
       version: "0.1.0",
       elixir: "~> 1.13",
+      lockfile: lockfile(current_elixir_version()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +22,29 @@ defmodule ProdMode.MixProject do
   defp deps do
     [
       {:sentry, path: "../.."},
-      {:finch, "~> 0.17"},
-      {:jason, "~> 1.1"}
+      {:finch, dep_version(:finch, current_elixir_version())},
+      {:jason, "~> 1.1"},
+      {:hpax, dep_version(:hpax, current_elixir_version()), override: true}
     ]
   end
+
+  defp current_elixir_version, do: Version.parse!(System.version())
+
+  defp dep_version(:finch, %Version{major: 1, minor: minor}) when minor < 15,
+    do: "~> 0.17 and < 0.22.0"
+
+  defp dep_version(:finch, %Version{}), do: "~> 0.17"
+
+  defp dep_version(:hpax, %Version{major: 1, minor: minor}) when minor < 15,
+    do: "~> 1.0.0 and < 1.0.4"
+
+  defp dep_version(:hpax, %Version{}), do: "~> 1.0"
+
+  defp lockfile(%Version{major: 1, minor: minor}) when minor < 15,
+    do: "mix-1.13-1.14.lock"
+
+  defp lockfile(%Version{major: 1, minor: minor}) when minor < 18,
+    do: "mix-1.15-1.17.lock"
+
+  defp lockfile(%Version{}), do: "mix.lock"
 end
