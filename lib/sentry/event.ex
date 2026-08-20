bd187f0e7381622d6e79ff2c76dc323cb593382b
Peter Solnica <peter@solnica.online>
2026-08-20 13:07:15 +0200

    refactor(tracing): Unify OpenTelemetry trace context extraction (#1177)

--- lib/sentry/event.ex
+++ lib/sentry/event.ex
@@ -502,32 +502,12 @@ defmodule Sentry.Event do
 
     case current_trace_context() do
       nil -> contexts
-      trace -> Map.put(contexts, :trace, trace)
+      {trace_id, span_id} -> Map.put(contexts, :trace, %{trace_id: trace_id, span_id: span_id})
     end
   end
 
   if Sentry.OpenTelemetry.VersionChecker.tracing_compatible?() do
-    defp current_trace_context do
-      case :otel_tracer.current_span_ctx() do
-        :undefined ->
-          nil
-
-        span_ctx ->
-          trace_id = :otel_span.trace_id(span_ctx)
-          span_id = :otel_span.span_id(span_ctx)
-
-          if trace_id != 0 and span_id != 0 do
-            %{trace_id: format_id(trace_id, 32), span_id: format_id(span_id, 16)}
-          end
-      end
-    end
-
-    defp format_id(id, length) do
-      id
-      |> Integer.to_string(16)
-      |> String.pad_leading(length, "0")
-      |> String.downcase()
-    end
+    defp current_trace_context, do: Sentry.OpenTelemetry.TraceContext.current()
   else
     defp current_trace_context, do: nil
   end
